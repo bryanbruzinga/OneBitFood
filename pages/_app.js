7763c15d7a69fb02ffa12a31955619085c396481
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-05-13 09:30:11 -0300

    página home concluída

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import Header from "../components/Header";
+import "../styles/globals.scss";
+import Container from "react-bootstrap/Container";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>OneBitFood V2</title>
+        <link ref="icon" href="/favicon.ico.png" />
+      </Head>
+      <main>
+        <Header />
+        <Container className="mt-5">
+          <Component {...pageProps} />
+        </Container>
+      </main>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
