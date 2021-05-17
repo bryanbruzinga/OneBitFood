b8943fcf50ed6d111f8bd21c9a956079a932c7e2
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-05-17 10:44:16 -0300

    app consome o backend

--- components/ListRestaurants/index.js
+++ components/ListRestaurants/index.js
@@ -1,58 +1,35 @@
-import { Row } from "react-bootstrap";
+import { Row, Col, Spinner, Alert } from "react-bootstrap";
 import Restaurant from "./Restaurant";
+import getRestaurants from "../../services/getRestaurants";
 
 export default function ListRestaurants() {
-  const restaurants = [
-    {
-      id: 1,
-      name: "example 1",
-      description: "lorem ipsum dolor sit amet, consectetur adipiscing",
-      delivery_tax: "5",
-      image_url: "/restaurant.jpeg",
-      category_title: "Cozinha japonesa",
-    },
-    {
-      id: 2,
-      name: "example 2",
-      description: "lorem ipsum dolor sit amet, consectetur adipiscing",
-      delivery_tax: "10",
-      image_url: "/restaurant.jpeg",
-      category_title: "Cozinha mineira",
-    },
-    {
-      id: 3,
-      name: "example 3",
-      description: "lorem ipsum dolor sit amet, consectetur adipiscing",
-      delivery_tax: "7",
-      image_url: "/restaurant.jpeg",
-      category_title: "Cozinha alienígena",
-    },
-    {
-      id: 4,
-      name: "example 4",
-      description: "lorem ipsum dolor sit amet, consectetur adipiscing",
-      delivery_tax: "2",
-      image_url: "/restaurant.jpeg",
-      category_title: "Cozinha tailandesa",
-    },
-    {
-      id: 5,
-      name: "example 5",
-      description: "lorem ipsum dolor sit amet, consectetur adipiscing",
-      delivery_tax: "5",
-      image_url: "/restaurant.jpeg",
-      category_title: "Cozinha nórdica",
-    },
-  ];
+  const { restaurants, isLoading, isError } = getRestaurants();
+
+  function renderContent() {
+    if (isError)
+      return (
+        <Col>
+          <Alert variant="custom-red">Erro ao carregar</Alert>
+        </Col>
+      );
+    else if (isLoading)
+      return (
+        <Col>
+          <Spinner animation="border" />
+        </Col>
+      );
+    else if (restaurants.length == 0)
+      return <Col>Nenhum restaurante disponível ainda...</Col>;
+    else
+      return restaurants.map((restaurant, i) => (
+        <Restaurant {...restaurant} key={i} />
+      ));
+  }
 
   return (
     <div className="mt-5">
       <h3 className="fw-bold">Restaurantes</h3>
-      <Row>
-        {restaurants.map((restaurant, i) => (
-          <Restaurant {...restaurant} key={i} />
-        ))}
-      </Row>
+      <Row>{renderContent()}</Row>
     </div>
   );
 }
